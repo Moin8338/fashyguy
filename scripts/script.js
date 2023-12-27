4f802e9302121b27b423142430728f022b8ee144
Moin8338 <Moinuddin@kadiwal.com>
2023-12-27 12:20:37 +0530

    10th commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,38 @@
 var open = document.getElementById('hamburger');
+var elements = document.getElementsByClassName('menu-list');
+var overlay = document.querySelector('.overlay');
+var nav = document.querySelector('nav');
+var icon = document.querySelector('.menu-toggle i');
 var changeIcon = true;
 
-open.addEventListener("click", function(){
+// Add click event listeners
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function () {
+        overlay.classList.toggle("menu-open");
+        nav.classList.toggle("menu-open");
+    
+        if (changeIcon) {
+    
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+    
+            changeIcon = false;
+        }
+        else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+            changeIcon = true;
+        }
+    });
+}
 
-    var overlay = document.querySelector('.overlay');
-    var nav = document.querySelector('nav');
-    var icon = document.querySelector('.menu-toggle i');
-
+open.addEventListener("click", function () {
+   
     overlay.classList.toggle("menu-open");
     nav.classList.toggle("menu-open");
 
     if (changeIcon) {
+
         icon.classList.remove("fa-bars");
         icon.classList.add("fa-times");
 
@@ -22,3 +44,7 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 });
+
+
+
+
